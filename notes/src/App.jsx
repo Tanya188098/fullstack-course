421703d268ad655a0a1c1dabb7ea99c86346863b
Tanya <t.zharova.dev@gmail.com>
2026-06-25 14:57:07 +0200

    chore: install axios and add script to package json

--- notes/src/App.jsx
+++ notes/src/App.jsx
@@ -1,6 +1,18 @@
 import Note from "./components/Note";
 
 const App = ({ notes }) => {
+  console.log("1");
+
+  setTimeout(() => {
+    console.log("2");
+  }, 100);
+
+  setTimeout(() => {
+    console.log("3");
+  }, 0);
+
+  console.log("4");
+
   return (
     <div>
       <h1>Notes</h1>
