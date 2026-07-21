b27c2bdc781b1c174236ba78c28112a03d627731
Tanya <t.zharova.dev@gmail.com>
2026-07-21 19:31:16 +0200

    feat: add morgan middleware

--- part3/backend/index.js
+++ part3/backend/index.js
@@ -1,8 +1,6 @@
 const express = require("express");
 const app = express();
 
-app.use(express.json());
-
 let notes = [
   {
     id: "1",
@@ -21,15 +19,36 @@ let notes = [
   },
 ];
 
+const requestLogger = (request, response, next) => {
+  console.log("Method:", request.method);
+  console.log("Path:  ", request.path);
+  console.log("Body:  ", request.body);
+  console.log("---");
+  next();
+};
+
+app.use(express.json());
+app.use(requestLogger);
+
 app.get("/", (request, response) => {
-  response.send("<h1>Hello World !</h1>");
+  response.send("<h1>Hello World!</h1>");
 });
 
 app.get("/api/notes", (request, response) => {
   response.json(notes);
 });
 
-//
+app.get("/api/notes/:id", (request, response) => {
+  const id = request.params.id;
+  const note = notes.find((note) => note.id === id);
+
+  if (note) {
+    response.json(note);
+  } else {
+    response.status(404).end();
+  }
+});
+
 const generateId = () => {
   const maxId =
     notes.length > 0 ? Math.max(...notes.map((n) => Number(n.id))) : 0;
@@ -56,23 +75,18 @@ app.post("/api/notes", (request, response) => {
   response.json(note);
 });
 
-app.get("/api/notes/:id", (request, response) => {
-  const id = request.params.id;
-  const note = notes.find((note) => note.id === id);
-  if (note) {
-    response.json(note);
-  } else {
-    response.status(404).end();
-  }
-});
-
-// deletion
 app.delete("/api/notes/:id", (request, response) => {
   const id = request.params.id;
   notes = notes.filter((note) => note.id !== id);
 
   response.status(204).end();
 });
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: "unknown endpoint" });
+};
+
+app.use(unknownEndpoint);
 
 const PORT = 3001;
 app.listen(PORT, () => {
--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,7 +1,18 @@
 const express = require("express");
+var morgan = require("morgan");
+
 const app = express();
 
 app.use(express.json());
+
+// logger
+morgan.token("body", (req) => {
+  return req.method === "POST" ? JSON.stringify(req.body) : "";
+});
+
+app.use(
+  morgan(":method :url :status :res[content-length] - :response-time :body"),
+);
 
 let persons = [
   {
