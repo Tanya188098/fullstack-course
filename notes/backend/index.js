59fb4c79e4e80db1c1cfbf6de01be761b97b6b5d
Tanya <t.zharova.dev@gmail.com>
2026-07-28 18:12:34 +0200

    chore: remove the route

--- notes/backend/index.js
+++ notes/backend/index.js
@@ -33,10 +33,6 @@ app.use(express.json());
 app.use(express.static("dist"));
 app.use(requestLogger);
 
-app.get("/", (request, response) => {
-  response.send("<h1>Hello World!</h1>");
-});
-
 app.get("/api/notes", (request, response) => {
   response.json(notes);
 });
